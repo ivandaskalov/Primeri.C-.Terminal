f8f31a81613eff0c6c9e0b8c9532aeae9cbfd66b
Иван Даскалов <ivandaskalov@abv.bg>
2015-10-14 17:12:42 +0300

    Модул 02, Видео 03 - Основни оператори за целочислен тип данни

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -11,37 +11,37 @@ namespace Modul02
 			int sum = 0;
 
 			//Събиране на числа
-			a= 10;
-			b = 3;
+			a = 10;
+			b = 5;
 
 			sum = a + b;
 
 			//Писане в конзола
-			Console.Write ( a );
+			Console.Write (a);
 			Console.Write (" + ");
-			Console.Write ( b );
+			Console.Write (b);
 			Console.Write (" = ");
-			Console.WriteLine ( sum ) ;
+			Console.WriteLine (sum);
 
 			//Допълнителни оператори
 			Console.WriteLine ("\nИзползване на +=");
 			a += b; //a = 10 + 5, a = a + b
-			Console.WriteLine ( a );
+			Console.WriteLine (a);
 
 			Console.WriteLine ("\nИзползване на -=");
 			a -= b; //a = 10 - 5, a = a - b
-			Console.WriteLine ( a );
+			Console.WriteLine (a);
 
 			Console.WriteLine ("\nИзползване на *=");
 			a *= b; //a = 10 * 5, a = a * b
-			Console.WriteLine ( a );
+			Console.WriteLine (a);
 
 			Console.WriteLine ("\nИзползване на ++");
-			a ++ ; //a = 10 + 5, a = a + b
-			Console.WriteLine ( a );
+			a++; //a = 50 + 1, a = a + b
+			Console.WriteLine (a);
 
-			Console.WriteLine ("Стандартно делене: " + (a / b).ToString()); // -> 2 (7 / 3 = 2,3333)
-			Console.WriteLine ("Остатък делене: " + (a % b).ToString()); // -> 1 (7 2*3 = 1)
+			Console.WriteLine ("Стандартно делене: " + (a / b).ToString ()); // -> a {(ab+1) / b = a + 1/b}
+			Console.WriteLine ("Остатък делене: " + (a % b).ToString()); // -> 1 {(ab+1) - ab = 1}
 		}
 	}
 }
