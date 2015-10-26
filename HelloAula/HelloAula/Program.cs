a9d57084ce6b928f4efce5cd44fcc4be0b18459f
Иван Даскалов <ivandaskalov@abv.bg>
2015-10-26 11:15:01 +0200

    Модул 02, Видео 07 - Работа с масиви

--- HelloAula/HelloAula/Program.cs
+++ HelloAula/HelloAula/Program.cs
@@ -8,6 +8,24 @@ namespace HelloAula
 		{
 			Console.WriteLine ("Hello My Aula!");
 //			Console.ReadKey ();
+
+			string ea = "Hello";
+			string eb = "World";
+			object ec;
+			string ed;
+
+			ec = ea + " " + eb;
+			ed = (string)ec;
+			Console.WriteLine(ec);
+			Console.WriteLine(ed);
+
+
+			int cifra = 150;
+			string slovom = "сто и петдесет";
+			string result = "Цифрата '" + cifra.ToString() + "' словом е '" + slovom + "'!";
+			int resultLenght = result.Length;
+			Console.WriteLine(result+", "+resultLenght);
+			Console.WriteLine("Цифрата '{0}' словом е '{1}'!, {2}", cifra, slovom,resultLenght);
 		}
 	}
 }
