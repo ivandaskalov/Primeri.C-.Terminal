1fc7814710f096f44a751613587a898fe9821f95
Иван Даскалов <ivandaskalov@abv.bg>
2015-11-26 15:20:30 +0200

    Модул 04, Видео 03 - Примери за влагане на цикли

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -6,13 +6,25 @@ namespace Modul04
 	{
 		public static void Main (string[] args)
 		{
-			string[] _table = {"Иван", "Гошо", "Гергана"};
+			string[,] _table = {
+				{ "Иван",		"Георгиев",		"19" },
+				{ "Васил",		"Иванов",		"23" },
+				{ "Милен",		"Георгиев",		"58" },
+				{ "Цветан",		"Бориславов",	"14" },
+				{ "Гергана",	"Василева",		"25" },
+				{ "Ивана",		"Христова",		"21" },
+			};
 
 			//Антетка за таблицата
-			Console.WriteLine ("Име");
-			Console.WriteLine ("=============");
-			for (int i = 0; i < _table.Length; i++) {
-				Console.WriteLine (_table[i]);
+			Console.WriteLine ("Име".PadRight(12) + "Фамилия".PadRight(12) + "Възраст".PadRight(12));
+			Console.WriteLine ("".PadRight(36,'='));
+
+			//Тяло на таблицата
+			for (int i = 0; i < _table.GetLength(0); i++) {
+				//Код на цикъла
+				for (int j = 0 ; j < _table.GetLength(1); j++)
+					Console.Write (_table[i,j].PadRight(12));
+				Console.WriteLine ();
 			}
 		}
 	}
