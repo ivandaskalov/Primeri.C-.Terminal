dafc924ea6e30a03b693d968602dcf779e0a6863
Иван Даскалов <ivandaskalov@abv.bg>
2015-11-26 12:19:10 +0200

    Модул 04, Видео 04 - Цикъл for

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 0; i <= 5; i+=2) {
+				Console.WriteLine (i.ToString());
+			}
+		}
+	}
+}
