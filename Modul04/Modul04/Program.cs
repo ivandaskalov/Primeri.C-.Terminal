7fce270d0f95f41f9551428a9899b09a19e8fce6
Иван Даскалов <ivandaskalov@abv.bg>
2015-11-26 16:23:28 +0200

    Модул 04, Видео 04 - Цикъл while

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -19,13 +19,22 @@ namespace Modul04
 			Console.WriteLine ("Име".PadRight(12) + "Фамилия".PadRight(12) + "Възраст".PadRight(12));
 			Console.WriteLine ("".PadRight(36,'='));
 
-			//Тяло на таблицата
-			for (int i = 0; i < _table.GetLength(0); i++) {
-				//Код на цикъла
-				for (int j = 0 ; j < _table.GetLength(1); j++)
-					Console.Write (_table[i,j].PadRight(12));
-				Console.WriteLine ();
+//			//Пример с for
+//			for (int i = 0; i < _table.GetLength(0); i++) {
+//				//Код на цикъла
+//				for (int j = 0 ; j < _table.GetLength(1); j++)
+//					Console.Write (_table[i,j].PadRight(12));
+//				Console.WriteLine ();
+//			}
+
+			//Пример с while
+			int i=0;
+			while(i < _table.GetLength(0))
+			{
+				Console.WriteLine (_table[i,0].PadRight(12) + _table[i,1].PadRight(12) + _table[i,2].PadRight(12));
+				i++;
 			}
+
 		}
 	}
 }
